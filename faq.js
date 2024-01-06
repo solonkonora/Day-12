e3b002a8965a41e2fe8f70acbf338c0cddbc221c
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-06 04:16:32 -0800

    add the js

--- faq.js
+++ faq.js
@@ -1,19 +1,8 @@
-const jokeEl = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
+const toggles = document.querySelectorAll('.faq-toggle')
 
-generateJoke()
-
-//using async await
-async function generateJoke () {
-  const config = {
-    headers: {
-      Accept: 'application/json',
-    },
-  }
-
-  const res = await fetch('https://icanhazdadjoke.com', config)
-
-  const data = await res.json()
-
-  jokeEl.innerHTML = data.joke
-}
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle
+    ('active')
+  })
+})
