7cbbeaca18c369450080e2f16361add01800e114
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-06 06:54:23 -0800

    fixing linter errors

--- faq.js
+++ faq.js
@@ -1,10 +1,6 @@
-
 const toggles = document.querySelectorAll('.faq-toggle')
-
 toggles.forEach(toggle => {
   toggle.addEventListener('click', () => {
-    toggle.parentNode.classList.toggle
-    ('active')
+    toggle.parentNode.classList.toggle('active')
   })
 })
-
